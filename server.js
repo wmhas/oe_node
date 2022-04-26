3a4864366d78cf5096fb3fcaf9b3f25345810f0f
wmhas <whashim@gmail.com>
2022-04-26 20:21:14 +0800

    add faker

--- server.js
+++ server.js
@@ -1,25 +1,13 @@
 var MongoClient = require('mongodb').MongoClient;
-//var faker = require("faker");
+var faker = require("faker");
  
 const uri = "mongodb://oeuser3:321ToughJob^@128.199.195.92:27017/osc1?retryWrites=true&w=majority";
  
  
-// var randomName = faker.name.findName(); // Generates a random name
-// var randomEmail = faker.internet.email(); // Generates a random email
-// var randomProduct = faker.commerce.productName(); // Generates a random product name
-// var randomCompany = faker.company.companyName(); // Will give back a random company name
-// var randomCard = faker.helpers.createCard(); // It's output is a random contact card containing many properties
+let firstName = faker.name.firstName();
+let lastName = faker.name.lastName();
 
-
-// for (i = 0; i < 20; i++) {
-// 	console.log(randomName); // Outputs a random name
-// 	console.log(randomEmail); // Outputs a random email
-// 	console.log(randomProduct); // Outputs the random product name generated
-// 	console.log(randomCompany); // Produces a random company name
-// 	console.log(randomCard); // Gives back a random card
-// 	console.log(faker.date.past()); // Generates a random past date
-// }
-
+console.log(`ewewewe: ${firstName} ${lastName}`);
 
  
 // MongoClient.connect(uri, function(err, db) {
