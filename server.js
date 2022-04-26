748017fc037b2fec9f792e6afca593a0cc822865
wmhas <whashim@gmail.com>
2022-04-26 22:51:43 +0800

    insert record

--- server.js
+++ server.js
@@ -1,28 +1,33 @@
 require('dotenv').config();
 var MongoClient = require('mongodb').MongoClient;
 var faker = require("faker");
+const { fi } = require('faker/lib/locales');
 
- const uri =  process.env.DB_URL
+const url =  process.env.DB_URL
  
 let firstName = faker.name.firstName();
-let lastName = faker.name.lastName();
+let lastName = faker.address.streetAddress();
 
 console.log(`TEST FAKER: ${firstName} ${lastName}`);
 
-console.log(process.env.USER_ID + uri);
+console.log(process.env.USER_ID + url);
 
- 
-// MongoClient.connect(uri, function(err, db) {
-//   if(!err) {
-//     console.log("We are connected");
+  
+MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("osc1");
+    var myobj = { name:firstName , address:lastName };
+    dbo.collection("customers").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        //db.close();
+    });
 
-//     var dbo = db.db("osc1");
-//     var myobj = { name: "Company oe 1 ", address: "CYBERJAYA" };
-//     dbo.collection("customers").insertOne(myobj, function(err, res) {
-//       if (err) throw err;
-//       console.log("1 document inserted");
-//       db.close();
-//     });
-// };
-// });
- 
+    dbo.collection("customers").find({}).toArray(function(err, result) {
+        if (err) throw err;
+        console.log(result);
+        db.close();
+      });
+
+
+  });
